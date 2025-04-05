5a1e54dd9f7bcb9e37669938a426f3e579576fd3
jakkrish <jak.krish@gmail.com>
2025-04-05 18:46:52 +0530

    syntax fixed

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,37 @@
 import { useReducer } from 'react';
 import './App.css';
 
+const INCREMENT = 'increment'
+const DECREMENT = 'decrement'
+
 function App() {
 
   const [state,dispatch] = useReducer(reducer, {value:0})
 
-  function reducer(action,payload){
-    switch(action){
-      case 'increment':  state.value++
+  function reducer(state,action){
+    switch(action.type){
+      case INCREMENT:  return{
+        ...state,
+        value:state.value+1
+      }
       break;
-      case 'decrement':  state.value++
+      case DECREMENT:  return{
+        ...state,
+        value : state.value-1
+      }
       break;
-
+      default: return{
+        ...state
+      }
+        
     }
   }
 
   return <>
     {state.value}
     <div>
-    <button onClick={()=>{dispatch("increment")}}>Increment</button>
-    <button onClick={()=>{dispatch("decrement")}}>Decrement</button>
+    <button onClick={()=>{dispatch({type:INCREMENT})}}>Increment</button>
+    <button onClick={()=>{dispatch({type:DECREMENT})}}>Decrement</button>
     </div>
 
   </>;
