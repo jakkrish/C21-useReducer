39de32c8155912cb884ad070a1abee824371d1b7
jakkrish <jak.krish@gmail.com>
2025-04-05 06:48:27 +0530

    functionality implemented

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,28 @@
+import { useReducer } from 'react';
 import './App.css';
 
 function App() {
-  return <></>;
+
+  const [state,dispatch] = useReducer(reducer, {value:0})
+
+  function reducer(action,payload){
+    switch(action){
+      case 'increment':  state.value++
+      break;
+      case 'decrement':  state.value++
+      break;
+
+    }
+  }
+
+  return <>
+    {state.value}
+    <div>
+    <button onClick={()=>{dispatch("increment")}}>Increment</button>
+    <button onClick={()=>{dispatch("decrement")}}>Decrement</button>
+    </div>
+
+  </>;
 }
 
 export default App;
